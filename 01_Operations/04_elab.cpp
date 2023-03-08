f36ac0439fcb4c6f4bdd280c5854d488e4f4193c
Shashank <110643754+shashank-pd@users.noreply.github.com>
2023-03-08 11:11:34 +0530

    Create 04_elab.cpp

---
+++ 01_Operations/04_elab.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int weightinearth;
+    float weightinmoon;
+    cin>>weightinearth;
+    weightinmoon=0.166*weightinearth;
+    cout<<weightinmoon;
+    return 0;
+}
